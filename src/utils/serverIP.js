b4831e155085579b8bd27ea068cd1ed99868c9ef
ZhangHuaQian <18260979237@163.com>
2020-06-05 19:42:11 +0800

    000

--- src/utils/serverIP.js
+++ src/utils/serverIP.js
@@ -2,4 +2,5 @@
 // export const serverip = 'http://172.31.7.15:8081' // 需要添加http://
 // export const serverip = 'http://127.0.0.1:8081'
 export const serverip = 'http://171.111.153.241:8095' // 需要添加http://
-// export const serverip = 'http://192.168.1.12:8087/' // 需要添加http:/
+// export const serverip = 'http://192.168.1.12:8087/' // 需要添加http://
+// export const serverip = 'http://192.168.1.31:8080/' // 需要添加http://
