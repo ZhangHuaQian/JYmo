4a461d0ae847a361cc61c82391adbf005a440790
ZhangHuaQian <18260979237@163.com>
2020-06-10 23:05:34 +0800

    0610最后一次更新

--- src/App.vue
+++ src/App.vue
@@ -4,29 +4,29 @@
   </div>
 </template>
 <script>
-import {serverIP as ip} from '@/utils/serverIP'
-export default {
-  methods: {
-    _isMobile() {
-      let flag = navigator.userAgent.match(
-        /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
-      );
-      return flag;
-    }
-  },
-  mounted() {
-    if (this._isMobile()) {
-      alert("手机端");
-      // this.$router.replace("/home");
-      // window.location.href="www.360.com"
-    } else {
-      alert("pc端页面，检测到非手机，临时跳转到百度");
-            // this.$router.replace("/home");
-window.location.replace("http://www.baidu.com");
+// import {serverIP as ip} from '@/utils/serverIP'
+// export default {
+//   methods: {
+//     _isMobile() {
+//       let flag = navigator.userAgent.match(
+//         /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
+//       );
+//       return flag;
+//     }
+//   },
+//   mounted() {
+//     if (this._isMobile()) {
+//       alert("手机端");
+//       // this.$router.replace("/home");
+//       // window.location.href="www.360.com"
+//     } else {
+//       alert("pc端页面，检测到非手机，临时跳转到百度");
+//             // this.$router.replace("/home");
+// window.location.replace("http://www.baidu.com");
 
-    }
-  }
-};
+//     }
+//   }
+// };
 </script>
 
 <style lang="scss">
